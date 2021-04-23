9c0aa6e35b9a9fc67b824b123189b6d5f14fa3ec
Jules Dubayle <julesdubaye@hotmail.com>
2021-04-23 14:10:03 +0200

    Test pour clean le CSV

--- test_panda.py
+++ test_panda.py
@@ -3,7 +3,6 @@ import csv
 import json
 from flask import Flask, render_template, url_for
 
-
 app = Flask(__name__)
 
 
@@ -11,20 +10,33 @@ app = Flask(__name__)
 def map_func():
     return render_template('home.html')
 
+
 @app.route('/leaflet')
 def leaflet():
-    Id = pd.read_csv('import_colonies_fev2021.csv', usecols=['secteur_nom_fr'], sep=';', encoding="utf8")
-    Long1 = pd.read_csv('import_colonies_fev2021.csv', usecols=['longitude'], sep=';')
-    Lat1 = pd.read_csv('import_colonies_fev2021.csv', usecols=['latitude'], sep=';')
+    namef = pd.read_csv('import_colonies_fev2021_OG.csv', usecols=['secteur_nom_fr'], sep=';', encoding="utf8")
+    longf = pd.read_csv('import_colonies_fev2021_OG.csv', usecols=['longitude'], sep=';')
+    latf = pd.read_csv('import_colonies_fev2021_OG.csv', usecols=['latitude'], sep=';')
 
-    dfid = pd.DataFrame(Id, columns=['secteur_nom_fr'])
-    dfx = pd.DataFrame(Long1, columns=['longitude'])
-    dfy = pd.DataFrame(Lat1, columns=['latitude'])
+    new_name = namef.dropna()
+    new_long = longf.dropna()
+    new_lat = latf.dropna()
 
-    ID = dfid.values.tolist()
-    Long = dfx.values.tolist()
-    Lat = dfy.values.tolist()
-    return render_template('leaflet.html', Long=json.dumps(Long), Lat=json.dumps(Lat), ID=json.dumps(ID))
+    for x in new_long.index:
+        if new_long.loc[x, "longitude"] > 180:
+            new_long.drop(x, inplace=True)
+
+    for x in new_lat.index:
+        if new_lat.loc[x, "latitude"] > 90:
+            new_lat.drop(x, inplace=True)
+
+    dfname = pd.DataFrame(new_name, columns=['secteur_nom_fr'])
+    dfx = pd.DataFrame(new_long, columns=['longitude'])
+    dfy = pd.DataFrame(new_lat, columns=['latitude'])
+
+    name = dfname.values.tolist()
+    long = dfx.values.tolist()
+    lat = dfy.values.tolist()
+    return render_template('leaflet.html', Long=json.dumps(long), Lat=json.dumps(lat), ID=json.dumps(name))
     """Id = pd.read_csv('ville.csv', usecols=['Name'],sep=';',encoding='utf8')
 	Long1 = pd.read_csv('ville.csv', usecols=['Long'],sep=';') 
 	Lat1 = pd.read_csv('ville.csv', usecols=['Lat'],sep=';') 
@@ -41,6 +53,3 @@ def leaflet():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
