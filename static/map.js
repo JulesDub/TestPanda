f2855d41922ce2c8019a9d381b7049ff0e9bd355
Jules Dubayle <julesdubaye@hotmail.com>
2021-04-26 15:17:48 +0200

    Autres paramètres pour empêcher de mauvaise lecture du csv et empêche d'autres erreurs

--- static/map.js
+++ static/map.js
@@ -1,17 +1,10 @@
 
-        function initialize() {
-            console.log(JSON);
-            var ID = JSON.parse('{{ ID | safe}}');
-            var Lat = JSON.parse('{{ Lat | safe}}');
-            var Long = JSON.parse('{{ Long | safe}}');
-            var map = L.map('map').setView([48.833, 2.333], 7);
-            for (var i = Lat.length -1 ; i>=0;i-- ) { //Créer un marqueur avec le nom[i], la latitude[i] et la longitude[i]
-                L.marker([ Lat[i][0] , Long[i][0]]).addTo(map) .bindTooltip( (ID[i][0]), {permanent: false, direction: 'top'});
-             }
-            var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { // LIGNE 20
-            attribution: '© OpenStreetMap contributors',
-            maxZoom: 19
-            });
-
-            map.addLayer(osmLayer);
-             }
+function initialize() {
+    var data = JSON.parse('{{ data | safe }}');
+    var map = L.map('map').setView([48.833, 2.333], 7);
+    for (var i = data.length-1 ; i>=0;i-- ) {
+        L.marker([ data[i][1] , data[i][2]]).addTo(map) .bindTooltip( (data[i][0]), {permanent: false, direction: 'top'});
+        }
+    var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors',maxZoom: 19 });
+    map.addLayer(osmLayer);
+}
--- test_panda.py
+++ test_panda.py
@@ -14,32 +14,16 @@ def map_func():
 @app.route('/leaflet')
 def leaflet():
     data = pd.read_csv('import_colonies_fev2021.csv', usecols=['secteur_nom_fr', 'longitude', 'latitude'], sep=';', encoding="utf8")
-    new_data = data.dropna()
-
+    new_data = data.replace(["'", '		'], [" ", ''], regex=True)#removing " ' " char and multiple spaces unwanted which create bug on json object
+    new_data = new_data.dropna()#for cleaning empty data in csv
     for x in new_data.index:
-        if new_data.loc[x, "longitude"] > 180:
+        if new_data.loc[x, "longitude"] > 180:#for cleaning wrong lng in csv
             new_data.drop(x, inplace=True)
-        if new_data.loc[x, "latitude"] > 90:
+        if new_data.loc[x, "latitude"] > 90:#for cleaning wronf lat in csv
             new_data.drop(x, inplace=True)
-
     dfdata = pd.DataFrame(new_data, columns=['secteur_nom_fr', 'latitude', 'longitude'])
-
     dataf = dfdata.values.tolist()
-
     return render_template('leaflet.html', data=json.dumps(dataf))
-    """Id = pd.read_csv('ville.csv', usecols=['Name'],sep=';',encoding='utf8')
-	Long1 = pd.read_csv('ville.csv', usecols=['Long'],sep=';') 
-	Lat1 = pd.read_csv('ville.csv', usecols=['Lat'],sep=';') 
-
-	dfid= pd.DataFrame(Id,columns=['Name'])
-	dfx= pd.DataFrame(Long1,columns=['Long'])
-	dfy= pd.DataFrame(Lat1,columns=['Lat'])
-
-	ID = dfid.values.tolist()
-	Long = dfx.values.tolist()
-	Lat = dfy.values.tolist()
-	return render_template('leaflet.html', ID=json.dumps(ID), Long=json.dumps(Long),Lat=json.dumps(Lat))"""
-
 
 if __name__ == '__main__':
     app.run(debug=True)
