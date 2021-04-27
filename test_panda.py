621470ea386f952d186116682b02a6c9d40439f6
JulesDub <82599862+JulesDub@users.noreply.github.com>
2021-04-27 12:03:56 +0200

    Affichage des deux CSV correct.

--- test_panda.py
+++ test_panda.py
@@ -13,6 +13,7 @@ def map_func():
 
 @app.route('/leaflet')
 def leaflet():
+    #Lecture du premier CSV
     data = pd.read_csv('import_colonies_fev2021.csv', usecols=['secteur_nom_fr', 'longitude', 'latitude'], sep=';', encoding="utf8")
     new_data = data.replace(["'", '		'], [" ", ''], regex=True)#removing " ' " char and multiple spaces unwanted which create bug on json object
     new_data = new_data.dropna()#for cleaning empty data in csv
@@ -23,7 +24,18 @@ def leaflet():
             new_data.drop(x, inplace=True)
     dfdata = pd.DataFrame(new_data, columns=['secteur_nom_fr', 'latitude', 'longitude'])
     dataf = dfdata.values.tolist()
-    return render_template('leaflet.html', data=json.dumps(dataf))
+    #Lecture du second CSV
+    datab = pd.read_csv('Export_Rinbio.csv', usecols=['QUADRIGE_ZONEMARINE', 'LONGITUDE', 'LATITUDE'], encoding="utf8")
+    new_datab = datab.replace(["'", '		'], [" ", ''], regex=True)  # removing " ' " char and multiple spaces unwanted which create bug on json object
+    new_datab = new_datab.dropna()  # for cleaning empty data in csv
+    for x in new_datab.index:
+        if new_datab.loc[x, "LONGITUDE"] > 180:  # for cleaning wrong lng in csv
+            new_datab.drop(x, inplace=True)
+        if new_datab.loc[x, "LATITUDE"] > 90:  # for cleaning wrong lat in csv
+            new_datab.drop(x, inplace=True)
+    dfdatab = pd.DataFrame(new_datab, columns=['QUADRIGE_ZONEMARINE', 'LATITUDE', 'LONGITUDE'])
+    databf = dfdatab.values.tolist()
+    return render_template('leaflet.html', datab=json.dumps(databf), data=json.dumps(dataf))
 
 if __name__ == '__main__':
     app.run(debug=True)
